f64f1e661b407bb71e3626e40aa98bd8f8935ecf
alsukkari.mohammad <alsukkari.mohammad@icloud.com>
2026-05-12 18:42:28 +0200

    DB done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const http = require('http');
 const { typeDefs } = require('./Schema/index');
 const { resolvers } = require('./resolvers/index');
 require('dotenv').config();
+const mongoose = require('mongoose');
 async function startApolloServer(typeDefs, resolvers){
     const app = express();
     const httpServer = http.createServer(app);
@@ -17,6 +18,9 @@ async function startApolloServer(typeDefs, resolvers){
     server.applyMiddleware({ app }); 
     await new Promise(resolve => httpServer.listen({ port: process.env.PORT }, resolve));
     console.log(`Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`);
+    await mongoose.connect(process.env.DB_URL);
+    console.log('Connected to MongoDB');
+    return { server, app };
 }
 
 startApolloServer(typeDefs, resolvers);
